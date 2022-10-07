a0aaf4134dae0a7aee11f51c172861d6dee7639f
usr <r114174@g.kobe-kosen.ac.jp>
2022-10-07 17:30:56 +0900

    TL hsv commit

--- src/color_detect_pub.py
+++ src/color_detect_pub.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python
 # -- coding: utf-8 --
-from time import sleep
-import pyrealsense2 as rs
 import numpy as np
 import cv2
 import rospy
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
+from std_msgs.msg import String
 
 class color_detect :
     def __init__(self):
@@ -15,8 +14,19 @@ class color_detect :
         self.bridge = CvBridge()
         rospy.init_node(self.node_name)
         self.image_pub = rospy.Publisher('output_image', Image, queue_size=10)
+        self.stop_pub = rospy.Publisher("stop", String, queue_size=10)
+        self.go_pub = rospy.Publisher("go", String, queue_size=10)
         self.image_sub = rospy.Subscriber("/usb_cam1/image_raw/compressed", CompressedImage, self.image_callback)
+        self.blue_threshold
+        self.blue_h 
+        self.blue_s
+        self.blue_v
+        self.red_threshold
+        self.red_h
+        self.red_s
+        self.red_v
 
+        
     def image_callback(self,image_msg):
         try:
                 np_arr = np.fromstring(image_msg.data, np.uint8)
@@ -25,59 +35,36 @@ class color_detect :
                 rospy.loginfo('err')
 
         output_image = self.process_image(input_image)
-
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(output_image, "8UC1"))
 
 
-
-
-    def process_image(self, frame):
-        
-        ### ここに加工処理などを記述する ###
+    def process_image(self, frame):### ここに加工処理などを記述する ###
         # フレーム待ち
         img = frame
-        #RGB    
-        red_R_min = 83
-        red_R_max = 255
-        red_G_min = 0
-        red_G_max = 50
-        red_B_min = 0
-        red_B_max = 100
-
-        R_min = 0
-        R_max = 0
-        G_min = 50
-        G_max = 255 
-        B_min = 30
-        B_max = 255
-
-        bgrLower = np.array([B_min, G_min, R_min])    # 抽出する色の下限(BGR)
-        bgrUpper = np.array([B_max, G_max, R_max])    # 抽出する色の上限(BGR)
-        img_mask = cv2.inRange(img, bgrLower, bgrUpper) # BGRからマスクを作成
-        
-        red_bgrLower = np.array([red_B_min, red_G_min, red_R_min])    # 抽出する色の下限(BGR)
-        red_bgrUpper = np.array([red_B_max, red_G_max, red_R_max]) 
-        red_img_mask = cv2.inRange(img, red_bgrLower, red_bgrUpper)
-
-
-        mask_1 = cv2.inRange(img, (0, 127, 127), (30, 255, 255))
-        mask_2 = cv2.inRange(img, (0, 0, 0), (255, 255, 255))
-        img_ = cv2.bitwise_or(mask_1, mask_2)
-
-        #論理演算で色検出
-        dst = cv2.bitwise_and(img_, img_, mask=img_mask)
-        red_dst = cv2.bitwise_and(img_, img_, mask=red_img_mask)
-
+        #色検出
+        blue_img_mask = cv2.inRange(img, np.array([80, 80,0]), np.array([115, 127, 200]))
+        red_img_mask = cv2.inRange(img, np.array([0, 0,76]), np.array([35, 127, 200]))
+        #色面積
+        redPixels = cv2.countNonZero(red_img_mask)
+        bluePixels = cv2.countNonZero(blue_img_mask)
+        if redPixels >=100:
+            rospy.loginfo('stop')
+            msg = "stop {}".format(rospy.get_time())
+            self.stop_pub.publish(msg)
+        if bluePixels >=100:
+            msg = "go {}".format(rospy.get_time())
+            self.go_pub.publish(msg)
+            rospy.loginfo('go')
         # 表示
-        images = np.hstack((dst))
         cv2.namedWindow('color_image', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('color_image', img)
-        cv2.namedWindow('detect_image', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('detect_image', dst)
+        cv2.namedWindow('blue_detect_image', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('blue_detect_image', blue_img_mask)
         cv2.namedWindow('red_detect_image', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('red_detect_image', red_dst)
+        cv2.imshow('red_detect_image', red_img_mask)
         cv2.waitKey(1)
-        return dst
+
+        return  red_img_mask
 
 
 if __name__ == '__main__':
--- src/detect_color_mp4.py
+++ src/detect_color_mp4.py
@@ -31,44 +31,14 @@ for i in range(frame_count):
 
         img = frames
 
-        #RGB    
-        red_R_min = 83
-        red_R_max = 255
-        red_G_min = 0
-        red_G_max = 50
-        red_B_min = 0
-        red_B_max = 100
-
-        R_min = 0
-        R_max = 0
-        G_min = 50
-        G_max = 255 
-        B_min = 30
-        B_max = 255
-
-        bgrLower = np.array([B_min, G_min, R_min])    # 抽出する色の下限(BGR)
-        bgrUpper = np.array([B_max, G_max, R_max])    # 抽出する色の上限(BGR)
-        img_mask = cv2.inRange(img, bgrLower, bgrUpper) # BGRからマスクを作成
-        
-        red_bgrLower = np.array([red_B_min, red_G_min, red_R_min])    # 抽出する色の下限(BGR)
-        red_bgrUpper = np.array([red_B_max, red_G_max, red_R_max]) 
-        red_img_mask = cv2.inRange(img, red_bgrLower, red_bgrUpper)
-
-
-        mask_1 = cv2.inRange(img, (0, 127, 127), (30, 255, 255))
-        mask_2 = cv2.inRange(img, (0, 0, 0), (255, 255, 255))
-        img_ = cv2.bitwise_or(mask_1, mask_2)
-
-        #論理演算で色検出
-        dst = cv2.bitwise_and(img_, img_, mask=img_mask)
-        red_dst = cv2.bitwise_and(img_, img_, mask=red_img_mask)
+        blue_img_mask =  cv2.inRange(img, np.array([80, 80,0]), np.array([115, 127, 200]))
+        red_img_mask = cv2.inRange(img, np.array([0, 0,76]), np.array([35, 127, 200]))
 
         # 表示
-        images = np.hstack((dst))
         cv2.namedWindow('color_image', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('color_image', img)
-        cv2.imshow('detect_image', dst)
-        cv2.imshow('red_detect_image', red_dst)
+        cv2.imshow('blue_detect_image', blue_img_mask)
+        cv2.imshow('red_detect_image',red_img_mask)
         sleep(0.01) 
     ### ここに加工処理などを記述する ###
 
