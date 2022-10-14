1f4666a363522e524505acd3f47c7cb1a5da7f48
usr <r114174@g.kobe-kosen.ac.jp>
2022-10-14 14:53:40 +0900

    見やすくしてみた

--- src/color_detect_pub.py
+++ src/color_detect_pub.py
@@ -2,11 +2,11 @@
 # -*- coding:utf-8 -*-
 
 from ast import Str
+from symbol import del_stmt
 import numpy as np
 import cv2
 import rospy
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 from std_msgs.msg import String
 
@@ -17,8 +17,8 @@ class color_detect :
         self.topic_name = "/camera/image_color"
         self.bridge = CvBridge()
         rospy.init_node(self.node_name)
-        self.red_image_pub = rospy.Publisher('red_output_image', Image, queue_size=10)
-        self.blue_image_pub = rospy.Publisher('blue_output_image', Image, queue_size=10)
+        self.red_image_pub = rospy.Publisher('out_put_image', Image, queue_size=10)
+        self.blue_image_pub = rospy.Publisher('intput_image', Image, queue_size=10)
         self.state_pub = rospy.Publisher("tl_state", String, queue_size=10)
         self.image_sub = rospy.Subscriber(self.topic_name, Image, self.image_callback)
 
@@ -43,17 +43,23 @@ class color_detect :
     def image_callback(self,image_msg):
         input_image = self.bridge.imgmsg_to_cv2(image_msg,"bgr8")
         output_image  = self.process_image(input_image)
-        self.red_image_pub.publish(self.bridge.cv2_to_imgmsg(output_image[0], "8UC1"))
-        self.blue_image_pub.publish(self.bridge.cv2_to_imgmsg(output_image[1], "8UC1"))
-
-
-    def process_image(self, frame):### ここに加工処理などを記述する ###
+        self.red_image_pub.publish(self.bridge.cv2_to_imgmsg(output_image[0], "rgb8"))
+        self.blue_image_pub.publish(self.bridge.cv2_to_imgmsg(output_image[1], "rgb8"))
+        
+    ### ここに加工処理などを記述する ###
+    def process_image(self, frame):
         # フレーム待ち
         img = frame
         #色検出
         blue_img_mask = cv2.inRange(img, np.array([self.blue_h_mim, self.blue_s_mim,self.blue_v_mim]), np.array([self.blue_h_max , self.blue_s_max , self.blue_v_max ]))
         red_img_mask = cv2.inRange(img, np.array([self.red_h_mim,self.red_s_mim,self.red_v_mim]), np.array([self.red_h_max,self.red_s_max, self.red_v_max]))
-        
+        color_red = (255, 0, 0)  # 2値画像で画素値255に割り当てる色
+        color_blue = (0, 0, 255)  # 2値画像で画素値255に割り当てる色
+        color_black = (0, 0, 0)  # 2値画像で画素値0に割り当てる色
+        dst_red =  np.where(red_img_mask[..., np.newaxis] == 255, color_red, color_black).astype(np.uint8)
+        dst_blue =  np.where(blue_img_mask[..., np.newaxis] == 255, color_blue, color_black).astype(np.uint8)
+        mask = np.bitwise_or( np.bitwise_or(dst_blue,dst_red),img)
+
         #色面積
         redPixels = cv2.countNonZero(red_img_mask)
         bluePixels = cv2.countNonZero(blue_img_mask)
@@ -68,8 +74,7 @@ class color_detect :
             state_msg.data = "unknown"
         self.state_pub.publish(state_msg)
 
-
-        return  [red_img_mask ,blue_img_mask]
+        return  [mask  ,img]
 
 
 if __name__ == '__main__':
